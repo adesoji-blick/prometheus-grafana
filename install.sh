fc1fc9f5f3c85d61b59b5b01a9b8a7e9cb105de5
adesoji <adesoji@blick.ng>
2021-09-23 12:41:07 -0600

    prometheus and grafana installation

--- install.sh
+++ install.sh
@@ -2,18 +2,17 @@
 
 ## Installing Prometheus
 echo ----- Installing Prometheus ------
-# cd ~
 wget https://github.com/prometheus/prometheus/releases/download/v2.30.0/prometheus-2.30.0.linux-amd64.tar.gz
 tar -xvf prometheus-2.30.0.linux-amd64.tar.gz
 cd prometheus-2.30.0.linux-amd64
 sudo cp -r . /usr/local/bin/prometheus
-cd ~
-sudo cp prometheus-grafana/prometheus.service /etc/systemd/system/prometheus.service
+sudo cp /home/ansible/prometheus-grafana/prometheus.service /etc/systemd/system/prometheus.service
 sudo systemctl enable prometheus
 sudo systemctl start prometheus
 
 
 ## Installing Node Exporter
+cd /home/ansible/
 echo ----- Installing Prometheus Node Exporter ------
 wget https://github.com/prometheus/node_exporter/releases/download/v1.2.2/node_exporter-1.2.2.linux-amd64.tar.gz
 tar xvf node_exporter-1.2.2.linux-amd64.tar.gz
@@ -25,8 +24,7 @@ sudo systemctl start node-exporter
 
 ## include Node-exporter job on prometheus.yml file and creating prometheus_rule.yml file
 echo ----- include Node-exporter job on prometheus.yml file and creating prometheus_rule.yml file -----
-cd ~
-# sudo rm /usr/local/bin/prometheus/prometheus.yml
+cd /home/ansible/
 sudo cp /home/ansible/prometheus-grafana/prometheus.yml /usr/local/bin/prometheus/prometheus.yml 
 sudo cp prometheus-grafana/prometheus_rules.yml  /usr/local/bin/prometheus/prometheus_rules.yml
 sudo systemctl restart prometheus
@@ -39,7 +37,7 @@ wget https://github.com/prometheus/alertmanager/releases/download/v0.23.0/alertm
 tar vxf alertmanager-0.23.0.linux-amd64.tar.gz
 cd alertmanager-0.23.0.linux-amd64/
 sudo cp -r . /usr/local/bin/alertmanager
-cd ~
+cd /home/ansible/
 sudo cp prometheus-grafana/alertmanager.service  /etc/systemd/system/alertmanager.service
 sudo rm /usr/local/bin/alertmanager/alertmanager.yml
 sudo cp prometheus-grafana/alertmanager.yml /usr/local/bin/alertmanager/alertmanager.yml
@@ -49,6 +47,7 @@ sudo systemctl start alertmanager
 
 ## Installing Grafana ---- port 3000
 echo ----- Installing grafana -------
+cd /home/ansible/
 wget https://dl.grafana.com/enterprise/release/grafana-enterprise-8.1.5-1.x86_64.rpm
 sudo yum install grafana-enterprise-8.1.5-1.x86_64.rpm -y
 sudo systemctl enable grafana-server
